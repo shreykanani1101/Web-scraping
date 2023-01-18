4ff4bed1335e2a05e6c96d07e2d9993048f898b8
shrey <shrey.kanani@darshan.ac.in>
2023-01-18 09:43:07 +0530

    Test 3

--- server.js
+++ server.js
@@ -5,156 +5,125 @@ const port = 3000;
 
 const request = require("request");
 const cheerio = require("cheerio");
-const ExcelJS = require("exceljs");
 const nodemailer = require("nodemailer");
 
-// const urls = [
-//   "https://www.ugc.ac.in/ugc_notices.aspx",
-//   "https://www.ugc.ac.in/ugc_circular.aspx",
-// ];
+const Sequelize = require("sequelize");
+
+const path = "mysql://root:admin@localhost:3306/ugc-notice-circular";
+
+const urls = [
+  "https://www.ugc.ac.in/ugc_notices.aspx",
+  "https://www.ugc.ac.in/ugc_circular.aspx",
+];
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
 
-  console.log("server running");
+  // console.log("server running");
 
-  setTimeout(() => {
-    notice();
-  }, 1000 * 5 * 60); 
+  const sequelize = new Sequelize(path, {
+    operatorsAliases: false,
+    logging: false,
+  });
 
-  function notice() {
-    setInterval(() => {
-      request(
-        "https://www.ugc.ac.in/ugc_notices.aspx",
-        async (err, response, html) => {
-          if (!err && response.statusCode == 200) {
-            const $ = cheerio.load(html);
+  let Record = sequelize.define("record", {
+    html: Sequelize.TEXT,
+    type: Sequelize.STRING,
+  });
+
+  setInterval(() => {
+    urls.forEach((url, i) => {
+      // console.log(url,i);
+      request(url, async (err, response, html) => {
+        if (!err && response.statusCode == 200) {
+          const $ = cheerio.load(html);
+          const stripped = $("#ctl00_bps_homeCPH_rptState")
+            .children()
+            .children()
+            .first()
+            .html()
+            .replace(/\s+/g, " ")
+            .trim();
+            
+          if (i == 0) {
+            Record
+              .findOne({ where: { type: "notice" } })
+              .then(async(record) => {
+                // console.log(record.get({ plain: true }));
+                let data = record.get({ plain: true });
+
+                if(data.html==stripped){
+                  // console.log("same");
+                }
+                else{
+                  console.log("new notice available✅");
+                  let id = await Record.update(
+                    { html:stripped },
+                    { where: { id: 1 } }
+                  );
+                  sendEmail();
+                }
+              })
+              .finally(() => {
+                // sequelize.close();
+                res.end("Running");
+              });
+          } else {
+            Record
+              .findOne({ where: { type: "circular" } })
+              .then(async(record) => {
+                // console.log(record.get({ plain: true }));
+                let data = record.get({ plain: true });
+                if(data.html==stripped){
+                  // console.log("same");
+                }
+                else{
+                  console.log("new circlular available✅");
+                  let id = await Record.update(
+                    { html:stripped },
+                    { where: { id: 2 } }
+                  );
+                  sendEmail();
+                }
+              })
+              .finally(() => {
+                // sequelize.close();
+                res.end("Running");
+              });
+          }
+
+          function sendEmail(){
+            var transporter = nodemailer.createTransport({
+              service: "gmail",
+              auth: {
+                user: "shrey.kanani@darshan.ac.in",
+                pass: "Darshan@spk1101",
+              },
+            });
   
-            //create excel
-            const workbook = new ExcelJS.Workbook();
+            var mailOptions = {
+              from: "shrey.kanani@darshan.ac.in",
+              to: "shreykanani5505@gmail.com",
+              subject: `⚠️UPDATE! New UGC ${i == 0 ? "Notice" : "Circular"}!`,
+              html: `<h3>UPDATE! UGC  ${
+                i == 0 ? "Notice" : "Circular"
+              }</h3><table><tr>${stripped}</tr></table>`,
+            };
   
-            workbook.xlsx.readFile("./UGC.xlsx").then(async function () {
-              let worksheet2 = workbook.getWorksheet("Notices_Latest_HTML");
-              worksheet2.columns = [{ header: "tag", key: "tag" }];
-  
-              // get scraptData
-              const stripped = $("#ctl00_bps_homeCPH_rptState")
-                .children()
-                .children()
-                .first()
-                .html()
-                .replace(/\s+/g, " ")
-                .trim();
-  
-              if (worksheet2.getCell("A2").value != stripped) {
-                console.log("Notice available!");
-                worksheet2.getCell("A2").value = stripped;
-  
-                await workbook.xlsx.writeFile("./UGC.xlsx");
-  
-                var transporter = nodemailer.createTransport({
-                  service: "gmail",
-                  auth: {
-                    user: "shrey.kanani@darshan.ac.in",
-                    pass: "Darshan@spk1101",
-                  },
-                });
-  
-                var mailOptions = {
-                  from: "shrey.kanani@darshan.ac.in",
-                  to: "shreykanani5505@gmail.com",
-                  subject: `⚠️UPDATE! New UGC Notice!`,
-                  html: `<h3>UPDATE! UGC Notice
-                  </h3><table><tr>${stripped}</tr></table>`,
-                };
-  
-                transporter.sendMail(mailOptions, function (error, info) {
-                  if (error) {
-                    console.log(error);
-                  } else {
-                    console.log("Email sent: " + info.response);
-                  }
-                });
+            transporter.sendMail(mailOptions, function (error, info) {
+              if (error) {
+                console.log(error);
               } else {
-                //console.log("No New Notices✅");
+                console.log("Email sent: " + info.response);
               }
             });
           }
         }
-      );
-    }, 1000 * 10 * 60);
-  }
-  
-
-  setTimeout(() => {
-    circular();
+      });
+    });
   }, 1000 * 10 * 60);
-
-  function circular() {
-    setInterval(() => {
-      request(
-        "https://www.ugc.ac.in/ugc_circular.aspx",
-        async (err, response, html) => {
-          if (!err && response.statusCode == 200) {
-            const $ = cheerio.load(html);
-
-            //create excel
-            const workbook = new ExcelJS.Workbook();
-
-            workbook.xlsx.readFile("./UGC.xlsx").then(async function () {
-              let worksheet2 = workbook.getWorksheet("Circulars_Latest_HTML");
-              worksheet2.columns = [{ header: "tag", key: "tag" }];
-
-              // get scraptData
-              const stripped = $("#ctl00_bps_homeCPH_rptState")
-                .children()
-                .children()
-                .first()
-                .html()
-                .replace(/\s+/g, " ")
-                .trim();
-
-
-              if (worksheet2.getCell("A2").value != stripped) {
-                console.log("Circular available!");
-                worksheet2.getCell("A2").value = stripped;
-
-                await workbook.xlsx.writeFile("./UGC.xlsx");
-
-                var transporter = nodemailer.createTransport({
-                  service: "gmail",
-                  auth: {
-                    user: "shrey.kanani@darshan.ac.in",
-                    pass: "Darshan@spk1101",
-                  },
-                });
-
-                var mailOptions = {
-                  from: "shrey.kanani@darshan.ac.in",
-                  to: "shreykanani5505@gmail.com",
-                  subject: `⚠️UPDATE! New UGC Circular!`,
-                  html: `<h3>UPDATE! UGC  Circular!
-                </h3><table><tr>${stripped}</tr></table>`,
-                };
-
-                transporter.sendMail(mailOptions, function (error, info) {
-                  if (error) {
-                    console.log(error);
-                  } else {
-                    console.log("Email sent: " + info.response);
-                  }
-                });
-              } else {
-                //console.log("No New Circular✅");
-              }
-            });
-          }
-        }
-      );
-    }, 1000 * 10 * 60);
-  }
-  res.end("running");
 });
 
 server.listen(port, hostname);
+
